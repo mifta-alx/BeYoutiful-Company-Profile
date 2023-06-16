71dda064816bbe1817092d8de903df429e396d3e
alx-cpu <alx.yoi67@gmail.com>
2023-06-16 20:56:46 +0700

    First Commit UI Landing Page

--- app/page.js
+++ app/page.js
@@ -1,113 +1,87 @@
 import Image from 'next/image'
+import { Plus_Jakarta_Sans } from 'next/font/google'
+const pjs = Plus_Jakarta_Sans({
+  subsets: ['latin'],
+  style: ['italic', 'normal'],
+  weight: ['200', '300', '400', '500', '600', '700', '800'],
+  display: 'swap'
+})
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex min-h-screen flex-col items-center justify-between bg-white" >
+      <div className='bg-gradient-to-tr from-rose-200 via-rose-100 to-white w-full h-screen'>
+
+        <nav class="bg-transparent w-full">
+          <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto md:py-12 md:px-16 p-4">
+            <a href="#" class="flex items-center">
+              <img src="./logo.png" class="h-12 mr-3" alt="Logo" />
+              <span class={`self-center text-2xl whitespace-nowrap text-black ${pjs.className} font-bold`}>Be<span className='text-primary opacity-[86%]'>You</span>tiful</span>
+            </a>
+            <button data-collapse-toggle="navbar-sticky" type="button" class="inline-flex items-center p-2 text-sm text-primary rounded-lg md:hidden focus:outline-none focus:ring-1 focus:ring-primary" aria-controls="navbar-sticky" aria-expanded="false">
+              <span class="sr-only">Open main menu</span>
+              <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+            </button>
+            <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+              <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-transparent md:flex-row md:space-x-8 md:mt-0 md:border-0 ">
+                <li>
+                  <a href="#" class="block py-2 pl-3 pr-4 text-primary opacity-[86%] md:p-0">Beranda</a>
+                </li>
+                <li>
+                  <a href="#" class="block py-2 pl-3 pr-4 text-primary opacity-[86%] md:p-0">Fasilitas</a>
+                </li>
+                <li>
+                  <a href="#" class="block py-2 pl-3 pr-4 text-primary opacity-[86%] md:p-0">Beauty Partner</a>
+                </li>
+                <li>
+                  <a href="#" class="block py-2 pl-3 pr-4 text-primary opacity-[86%] md:p-0">Contact</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+
+        <div className='flex max-w-screen-xl md:flex-row flex-col gap-8 md:gap-0 px-4 py-8 md:px-16 md:py-10 w-full items-center justify-between mx-auto'>
+          <div className='max-w-xl'>
+            <h1 className={`${pjs.className} text-xl md:text-4xl text-black font-bold`}>Discover your inner beauty with beYoutiful: Your ultimate beauty solution platform.</h1>
+            <div className='max-w-xs md:mt-6 mt-4'>
+              <p className={`${pjs.className} font-normal text-sm md:text-xl text-black`}>1st Indonesian Beauty Platform with Augmented Reality (AR) Fitur</p>
+            </div>
+            <div className={`bg-primary w-fit rounded-md md:px-6 md:py-3 px-4 py-2.5 uppercase opacity-[86px] md:mt-6 mt-4`}>
+              <p className={`${pjs.className} font-normal md:text-sm text-xs`}>Shop Now</p>
+            </div>
+          </div>
+          <img src='./banner.jpg' className='w-[428px] md:h-[500px] h-[250px] object-cover object-center md:rounded-tl-[47px] md:rounded-br-[47px] md:rounded-bl-[0px] md:rounded-tr-[47px] rounded-2xl' />
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className='bg-gradient-to-br from-rose-200 via-rose-100 to-white w-full h-screen items-center flex'>
+        <div className='flex max-w-screen-xl md:flex-row flex-col gap-8 md:gap-0 px-4 py-8 md:px-16 md:py-10 w-full items-center justify-between mx-auto'>
+          <img src='./section2.jpg' className='w-[468px] md:h-[540px] h-[250px] object-cover object-center md:rounded-tl-[47px] md:rounded-br-[47px] md:rounded-bl-[0px] md:rounded-tr-[47px] rounded-2xl' />
+          <div className='max-w-xl'>
+            <div className='space-y-5'>
+              <h1 className={`${pjs.className} text-xl md:text-5xl text-black font-normal italic`}>Modern Product for</h1>
+              <h1 className={`${pjs.className} text-xl md:text-5xl text-black font-bold not-italic`}>Modern Care</h1>
+            </div>
+            <div className='md:mt-6 mt-4'>
+              <p className={`${pjs.className} font-normal text-sm md:text-xl text-black text-justify`}>BeYoutiful adalah sebuah platform inovatif yang menyediakan solusi kecantikan yang lebih mudah dan efektif bagi konsumen. Platform kami dirancang untuk meningkatkan aksesibilitas, efisiensi bisnis, dan pengalaman pelanggan dalam mencari, memilih, dan membeli produk atau jasa kecantikan. Melalui teknologi canggih dan fitur-fitur inovatif, BeYoutiful menyediakan solusi kecantikan yang efektif dan sesuai dengan kebutuhan konsumen, serta mendorong inovasi dalam industri kecantikan</p>
+            </div>
+          </div>
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      {/* <div className='bg-gradient-to-tr from-rose-200 via-rose-100 to-white w-full h-screen items-center flex'>
+        <div className='flex max-w-screen-xl md:flex-row flex-col gap-8 md:gap-0 px-4 py-8 md:px-16 md:py-10 w-full items-center justify-between mx-auto'>
+          <img src='./section2.jpg' className='w-[468px] md:h-[540px] h-[250px] object-cover object-center md:rounded-tl-[47px] md:rounded-br-[47px] md:rounded-bl-[0px] md:rounded-tr-[47px] rounded-2xl' />
+          <div className='max-w-xl'>
+            <div className='space-y-5'>
+              <h1 className={`${pjs.className} text-xl md:text-5xl text-black font-normal italic`}>Modern Product for</h1>
+              <h1 className={`${pjs.className} text-xl md:text-5xl text-black font-bold not-italic`}>Modern Care</h1>
+            </div>
+            <div className='md:mt-6 mt-4'>
+              <p className={`${pjs.className} font-normal text-sm md:text-xl text-black text-justify`}>BeYoutiful adalah sebuah platform inovatif yang menyediakan solusi kecantikan yang lebih mudah dan efektif bagi konsumen. Platform kami dirancang untuk meningkatkan aksesibilitas, efisiensi bisnis, dan pengalaman pelanggan dalam mencari, memilih, dan membeli produk atau jasa kecantikan. Melalui teknologi canggih dan fitur-fitur inovatif, BeYoutiful menyediakan solusi kecantikan yang efektif dan sesuai dengan kebutuhan konsumen, serta mendorong inovasi dalam industri kecantikan</p>
+            </div>
+          </div>
+        </div>
+      </div> */}
     </main>
   )
 }
