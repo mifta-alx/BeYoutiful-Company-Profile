c802a2d5b948454c232904297227b66a8be4ba11
alx-cpu <alx.yoi67@gmail.com>
2023-06-18 11:12:59 +0700

    Update Product Section and Footer

--- app/page.js
+++ app/page.js
@@ -66,11 +66,13 @@ export default function Home() {
       'price': 'Rp60.000'
     },
   ]
+  const [toggle, setToggle] = useState(false)
   const [currentImageIndexes, setCurrentImageIndexes] = useState([0, 1, 2]);
+  const [currentIndex, setCurrentIndex] = useState(0);
   const [activeDotIndex, setActiveDotIndex] = useState(0);
 
   useEffect(() => {
-    const interval = setInterval(() => {
+    const intervalGroup = setInterval(() => {
       setCurrentImageIndexes((prevIndexes) => {
         const nextIndex = (prevIndexes[2] + 1) % dataImage.length;
         const nextIndexes = [];
@@ -85,7 +87,14 @@ export default function Home() {
       });
     }, 4000); // Mengubah interval menjadi 4 detik
 
-    return () => clearInterval(interval);
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % dataImage.length);
+    }, 3000);
+
+    return () => {
+      clearInterval(intervalGroup);
+      clearInterval(interval);
+    };
   }, []);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between bg-white" >
@@ -97,12 +106,12 @@ export default function Home() {
               <img src="./logo.png" className="h-12 mr-3" alt="Logo" />
               <span className={`self-center text-2xl whitespace-nowrap text-black ${pjs.className} font-bold`}>Be<span className='text-primary opacity-[86%]'>You</span>tiful</span>
             </a>
-            <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 text-sm text-primary rounded-lg md:hidden focus:outline-none focus:ring-1 focus:ring-primary" aria-controls="navbar-sticky" aria-expanded="false">
+            <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 text-sm text-primary rounded-lg md:hidden focus:outline-none focus:ring-1 focus:ring-primary" aria-controls="navbar-sticky" aria-expanded="false" onClick={() => setToggle(!toggle)}>
               <span className="sr-only">Open main menu</span>
               <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
             </button>
-            <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
-              <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-transparent md:flex-row md:space-x-8 md:mt-0 md:border-0 ">
+            <div className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${toggle ? '' : 'hidden'}`} id="navbar-sticky">
+              <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-primary bg-white rounded-lg bg-transparent md:flex-row md:space-x-8 md:mt-0 md:border-0 ">
                 <li>
                   <a href="#" className="block py-2 pl-3 pr-4 text-primary opacity-[86%] md:p-0">Beranda</a>
                 </li>
@@ -142,16 +151,26 @@ export default function Home() {
               <h1 className={`${pjs.className} text-3xl md:text-5xl text-black font-normal italic md:text-left text-center`}>Modern Product for</h1>
               <h1 className={`${pjs.className} text-3xl md:text-5xl text-black font-bold not-italic md:text-left text-center`}>Modern Care</h1>
             </div>
-            <div className='md:mt-6 mt-4'>
+            <div className='md:mt-6 mt-8'>
               <p className={`${pjs.className} font-normal text-sm md:text-xl text-black text-justify`}>BeYoutiful adalah sebuah platform inovatif yang menyediakan solusi kecantikan yang lebih mudah dan efektif bagi konsumen. Platform kami dirancang untuk meningkatkan aksesibilitas, efisiensi bisnis, dan pengalaman pelanggan dalam mencari, memilih, dan membeli produk atau jasa kecantikan. Melalui teknologi canggih dan fitur-fitur inovatif, BeYoutiful menyediakan solusi kecantikan yang efektif dan sesuai dengan kebutuhan konsumen, serta mendorong inovasi dalam industri kecantikan</p>
             </div>
           </div>
         </div>
       </div>
       <div className='bg-gradient-to-tr from-rose-200 via-rose-100 to-white w-full h-fit flex'>
-        <div className='flex max-w-screen-xl flex-col gap-8 md:gap-0 px-4 py-8 md:px-16 md:py-10 w-full items-center justify-center mx-auto'>
-          <h1 className={`uppercase ${pjs.className} font-medium tracking-widest text-black text-5xl`}>Product</h1>
-          <div className="flex justify-center space-x-6 mt-14">
+        <div className='flex max-w-screen-xl flex-col gap-4 md:gap-0 px-4 py-8 md:px-16 md:py-10 w-full items-center justify-center mx-auto'>
+          <h1 className={`uppercase ${pjs.className} font-medium tracking-widest text-black text-3xl md:text-5xl`}>Product</h1>
+          <div className="md:hidden flex justify-center flex-col items-center space-y-6 mt-2">
+            <img src={dataImage[currentIndex].url} alt="Product Image" className="w-[348px] h-[480px] object-cover rounded-2xl drop-shadow-xl" />
+            <div className="mt-8 bg-primary w-full rounded-xl px-6 py-4 flex flex-col gap-4 opacity-[86%]">
+              <span className="text-white space-x-4">
+                {'★'.repeat(dataImage[currentIndex].rating)}
+              </span>
+              <h3 className={`${pjs.className} font-semibold tracking-wide text-lg`}>{dataImage[currentIndex].productName}</h3>
+              <span className={`${pjs.className} text-base font-light text-white tracking-wide`}>{dataImage[currentIndex].price}</span>
+            </div>
+          </div>
+          <div className="md:flex hidden justify-center space-x-6 mt-14">
             {currentImageIndexes.map((index) => {
               const product = dataImage[index];
               return (
@@ -174,11 +193,11 @@ export default function Home() {
             }
             )}
           </div>
-          <div className="flex mt-8">
+          <div className="md:flex mt-8 hidden">
             {[0, 1, 2].map((index) => (
               <div
                 key={index}
-                className={`w-4 h-4 mx-1 rounded-full border-primary border ${index === activeDotIndex ? 'bg-primary' : 'bg-white'
+                className={`w-4 h-4 mx-1 rounded-full border-primary border ${index === activeDotIndex ? 'bg-primary' : 'bg-transparent'
                   }`}
               ></div>
             ))}
@@ -186,13 +205,13 @@ export default function Home() {
         </div>
       </div>
       <div className='bg-gradient-to-tr from-rose-200 via-rose-300 to-rose-400 w-full h-fit flex'>
-        <div className='flex max-w-screen-xl flex-col md:gap-20 px-4 py-8 md:px-16 md:py-10 w-full items-center justify-center mx-auto'>
-          <div className='flex flex-row justify-between w-full'>
+        <div className='flex max-w-screen-xl flex-col md:gap-20 gap-8 px-4 py-8 md:px-16 md:py-10 w-full items-center justify-center mx-auto'>
+          <div className='flex flex-col md:flex-row justify-between w-full gap-6 md:gap-0'>
             <div className='flex flex-col max-w-sm justify-between'>
               <h1 className={`text-2xl whitespace-nowrap text-black ${pjs.className} font-bold`}>Be<span className='text-primary opacity-[86%]'>You</span>tiful</h1>
               <p className={`${pjs.className} text-lg font-bold text-white`}>The No. 1 Digital Destination for all things about Beauty - Shopping, LifeStyle & Trends</p>
             </div>
-            <div className='flex flex-row gap-9'>
+            <div className='flex flex-col md:flex-row gap-9'>
               <div className={`flex flex-col ${pjs.className} gap-6`}>
                 <p className='uppercase font-normal'>information</p>
                 <ul className={`${pjs.className} font-normal text-sm`}>
@@ -246,9 +265,9 @@ export default function Home() {
               </div>
             </div>
           </div>
-          <div className='text-center w-full space-y-2'>
-            <h1 className={`${pjs.className} font-black text-5xl tracking-wider uppercase italic text-black`}>Never <span className='text-white'>miss a</span> sale <span className='text-white'>again</span></h1>
-            <p className={`${pjs.className} font-light text-white text-lg tracking-widest`}>by : LEGAR COMPANY</p>
+          <div className='text-center w-full space-y-2 '>
+            <h1 className={`${pjs.className} font-black text-2xl md:text-5xl tracking-wider uppercase italic text-black`}>Never <span className='text-white'>miss a</span> sale <span className='text-white'>again</span></h1>
+            <p className={`${pjs.className} font-light text-white text-base md:text-lg tracking-widest`}>by : LEGAR COMPANY</p>
           </div>
         </div>
       </div>
